a9c627c65b8d2b81970949b6fa2b28bd58974e95
Pablo Alcala <148588239+AlkaDev1@users.noreply.github.com>
2026-03-27 21:49:57 -0600

    Se integro la fuente Sugoe UI a las pantallas del sistema

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
-import pygler
+import pyglet
 
 ruta_fuente = "assets/fonts/segoeui.ttf"
-pygler.font.add_file(ruta_fuente)
+pyglet.font.add_file(ruta_fuente)
 
 from ui.styles import PALETA, MEDIDAS, configurar_estilos
 
